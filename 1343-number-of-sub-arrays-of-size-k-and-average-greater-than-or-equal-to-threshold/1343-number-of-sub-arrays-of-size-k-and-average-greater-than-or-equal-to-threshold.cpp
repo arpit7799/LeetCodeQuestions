d1d2ff0e0cd79139fa5ec45a9e6579abbed26ff5
Arpit Pandey <arp779921@gmail.com>
2026-08-30 19:02:21 +0530

    Time: 4 ms (15.37%), Space: 59.1 MB (70.8%) - LeetHub

---
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) 
+    {
+        int ans = 0;
+        int r = 0;
+        int s = 0;
+        for(r;r<k;r++)
+        {
+            s+=arr[r];
+        }
+        if(s/k >= threshold) ans++;
+        int l = 0;
+        while(r<arr.size())
+        {
+            s+=arr[r];
+            s-=arr[l];
+            if(s/k >= threshold) ans++;
+            l++;
+            r++;
+        }
+        return ans;
+    }
+//please upvote...
+};
